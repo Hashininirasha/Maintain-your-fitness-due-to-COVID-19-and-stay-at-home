628dc2bcaa06a27e1f4fc1be10bc066dd60ac0f6
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2021-01-14 20:42:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -759,6 +759,10 @@ People can't go out to meet his or her Doctor to get nutritional advice. Sometim
     <script src="js/bootstrap.min.js"></script>
     <!-- ALL PLUGINS -->
     <script src="js/custom.js"></script>
+		
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
